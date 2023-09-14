9c918e6c1de776cfb3f53f2ee7aff4109a56778d
Ahmed Amine Doudech <ahmedaminedoudech@gmail.com>
2023-09-14 23:08:21 +0100

    section 16

---
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { FC } from "react";
+
+interface RestaurantNavBarProps {}
+
+const RestaurantNavBar: FC<RestaurantNavBarProps> = ({}) => {
+  return (
+    <nav className="flex text-reg border-b pb-2">
+      <Link href="/restaurant/milestones-grill" className="mr-7">
+        {" "}
+        Overview{" "}
+      </Link>
+      <Link href="/restaurant/milestones-grill/menu" className="mr-7">
+        {" "}
+        Menu{" "}
+      </Link>
+    </nav>
+  );
+};
+
+export default RestaurantNavBar;
